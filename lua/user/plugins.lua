42c3f3e69c7029d8cacdffdc8489bba8401d3acb
lucaspari <lucas.pari@icloud.com>
2023-09-23 13:36:14 -0300

    adding my nvim files

---
+++ lua/user/plugins.lua
@@ -0,0 +1,36 @@
+lvim.plugins = {
+  {
+    'jwalton512/vim-blade',
+    ft = "blade",
+  },
+  {"github/copilot.vim"},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  ,
+  { 'mattn/emmet-vim' },
+  -- lazy.nvim
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup(
+        {
+          openai_params = {
+            model = "gpt-3.5-turbo",
+            frequency_penalty = 0,
+            presence_penalty = 0,
+            max_tokens = 300,
+            temperature = 0,
+            top_p = 1,
+            n = 1,
+          },
+        }
+      )
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
+  , -- lazy.nvim
+}
